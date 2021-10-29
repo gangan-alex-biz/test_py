5bec18689b7399dfc9c210e9bd2e98aac143e0f6
oleksandrgangan <gangan.alex.biz@gmail.com>
2021-10-29 17:14:42 +0300

    finalized python part: 3h 15 m

--- csvobtainer.py
+++ csvobtainer.py
@@ -1,6 +1,9 @@
-"""Reading CSV file, writing its first 5 columns and 10 rows to a news csv or json file"""
-from os.path import exists
-from pathlib import Path
+"""
+Reading CSV file, writing its first 5 columns and 10 rows to a news csv or json file.
+All the config is placed into settings.yml file.
+"""
+import logging
+import os
 from typing import Dict
 
 import pandas as pd
@@ -15,18 +18,26 @@ class FileGetter:
         self.cache: bool = cache
 
     def get_file(self) -> None:
+        if not self.cache:
+            logging.warning("Caching source file is disabled in the settings")
         if not self.is_file_cached or not self.cache:
-            req = requests.get(self.url)
-            with open(f"{self.cache_folder}/{self.file_name}", "wb") as file:
-                file.write(req.content)
+            try:
+                req = requests.get(self.url)
+                with open(self.file_name, "wb") as file:
+                    file.write(req.content)
+                logging.info("Downloaded fresh copy of the source file")
+            except:
+                logging.error("Exception occurred", exc_info=True)
+        else:
+            logging.info("Using previously downloaded file")
 
     @property
     def file_name(self) -> str:
-        return self.url.split("/")[-1].split("?")[0]
+        return f"{self.cache_folder}/{self.url.split('/')[-1].split('?')[0]}"
 
     @property
     def is_file_cached(self) -> bool:
-        return exists(f"{self.cache_folder}/{self.file_name}")
+        return os.path.exists(self.file_name)
 
 
 class FileTransformer:
@@ -34,14 +45,18 @@ class FileTransformer:
         self.file_name: str = file_name
 
     def read_first_cols_rows(self, cols: int, rows: int) -> pd.DataFrame:
-        df = pd.read_csv(self.file_name)
+        try:
+            df = pd.read_csv(self.file_name)
+        except:
+            logging.error("Exception occurred", exc_info=True)
+        logging.info(f"Extracted {cols} columns and {rows} rows")
         return df.iloc[:rows, :cols]
 
 
 class FileSaver:
-    def __init__(self, source_df, output_file_name, output_format):
+    def __init__(self, source_df, output_folder, output_file_name, output_format):
         self.source_df: pd.DataFrame = source_df
-        self.dest: str = f"{output_file_name[:-4]}_modified"
+        self.dest: str = f"{output_folder}/{output_file_name[:-4]}_modified"
         self.output_format: str = output_format
         self.formats: Dict[str] = {
             "json": self.save_as_json,
@@ -57,9 +72,12 @@ class FileSaver:
     def save(self) -> None:
         saver = self.formats.get(self.output_format, None)
         if saver:
-            saver()
+            try:
+                saver()
+            except:
+                logging.info(f"Saved to {self.dest}.{self.output_format} file")
         else:
-            raise Exception("Unknown output format")
+            logging.exception("Exception occurred: Unknown output format")
 
 
 def read_settings() -> dict:
@@ -68,15 +86,24 @@ def read_settings() -> dict:
     return settings
 
 
-def prepare_folders(settings) -> None:
-    Path(settings["cache_folder"]).mkdir(parents=True, exist_ok=True)
-    Path(settings["output_destination"]).mkdir(parents=True, exist_ok=True)
+def prepare_folder(folder: str) -> None:
+    if folder and not os.path.exists(folder):
+        os.makedirs(folder)
 
 
 if __name__ == "__main__":
 
     settings = read_settings()
-    prepare_folders(settings)
+    for folder in ["cache_folder", "output_destination", "logs_folder"]:
+        prepare_folder(settings[folder])
+
+    logging.basicConfig(
+        filename=f"{settings['logs_folder']}/log.txt",
+        level=logging.INFO,
+        format="%(asctime)s - %(message)s",
+    )
+
+    logging.info("Script started")
 
     getter = FileGetter(settings["url"], settings["cache_folder"], settings["cache"])
     getter.get_file()
@@ -87,7 +114,7 @@ if __name__ == "__main__":
         rows=settings["rows"],
     )
 
-    output_file_name = f"{settings['output_destination']}/{getter.file_name}"
-
-    saver = FileSaver(df, output_file_name, settings["output_format"])
+    saver = FileSaver(
+        df, settings["output_destination"], getter.file_name, settings["output_format"]
+    )
     saver.save()
